1b62a145a184c95c0be3e686e6ff3de61bc8e35e
Vikary Toby <vikaryto@student.21-school.ru>
2025-07-16 21:01:13 +0700

    feat: add activate.sh script

---
+++ activate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+if grep -q free-my-mac ~/.zshrc; then
+    echo -e "[${RED}ERR${NC}] Alias already exists in ~/.zshrc"
+else
+    echo "alias free-my-mac='$(pwd)/mac-cleaner.sh'" >> ~/.zshrc
+    echo -e "[${GREEN}OK${NC}] Alias added to ~/.zshrc"
+    echo -e "[${BLUE}INFO${NC}] Restart terminal or run: source ~/.zshrc"
+fi
