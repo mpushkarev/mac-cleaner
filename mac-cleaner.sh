6e55bac59d1f2bfb8b3559340591b4171ec1b2bd
Vikary Toby <vikaryto@student.21-school.ru>
2025-07-16 21:17:17 +0700

    feat: show info when nothing freed

--- mac-cleaner.sh
+++ mac-cleaner.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
 FREE_BEFORE=$(df -k ~ | tail -1 | awk '{print $4}')
 rm -rf ~/Library/Caches/*
 rm -rf ~/.Trash/*
 FREE_AFTER=$(df -k ~ | tail -1 | awk '{print $4}')
 
-FREED_KB=$(($FREE_AFTER - $FREE_BEFORE))
+FREED_KIB=$(($FREE_AFTER - $FREE_BEFORE))
 
-if [ "$FREED_KB" -lt 1024 ]; then
-  FREED="${FREED_KB} KiB"
+if [ "$FREED_KIB" -lt 1024 ]; then
+    FREED="${FREED_KIB} KiB"
 else
-  FREED_MIB=$(echo "scale=2; $FREED_KB / 1024" | bc)
-  FREED="${FREED_MIB} MiB"
+    FREED_MIB=$(echo "scale=2; $FREED_KIB / 1024" | bc)
+    FREED="${FREED_MIB} MiB"
 fi
 
-echo "Freed $FREED"
+if [ "$FREED_KIB" -le 0 ]; then
+    echo -e "[${BLUE}INFO${NC}] Nothing to clean, no disk space freed"
+else
+    echo -e "[${GREEN}OK${NC}] Freed $FREED"
+fi
