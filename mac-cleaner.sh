b4cc60c63899b4aba667db921c595822bdf6fb1e
Vikary Toby <vikaryto@student.21-school.ru>
2025-07-16 20:14:32 +0700

    feat: display freed disk space in Kiß/MiB after cleanup

--- mac-cleaner.sh
+++ mac-cleaner.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+FREE_BEFORE=$(df -k ~ | tail -1 | awk '{print $4}')
 rm -rf ~/Library/Caches/*
 rm -rf ~/.Trash/*
+FREE_AFTER=$(df -k ~ | tail -1 | awk '{print $4}')
+
+FREED_KB=$(($FREE_AFTER - $FREE_BEFORE))
+
+if [ "$FREED_KB" -lt 1024 ]; then
+  FREED="${FREED_KB} KiB"
+else
+  FREED_MIB=$(echo "scale=2; $FREED_KB / 1024" | bc)
+  FREED="${FREED_MIB} MiB"
+fi
+
+echo "Freed $FREED"
