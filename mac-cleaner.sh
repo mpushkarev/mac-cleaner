df86a652b4a197283a2cfa8076a446e2d8989225
Vikary Toby <vikaryto@student.21-school.ru>
2025-07-31 20:47:46 +0700

    feat: add --purge mode and extend mac cleanup directories

--- mac-cleaner.sh
+++ mac-cleaner.sh
@@ -6,6 +6,21 @@ YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
+PURGE=false
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -p|--purge)
+            PURGE=true
+            shift
+            ;;
+        *)
+            echo -e "[${RED}ERR${NC}] Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
 echo -e "[${YELLOW}RUN${NC}] Working..."
 
 FREE_BEFORE=$(df -k ~ | tail -1 | awk '{print $4}')
@@ -14,6 +29,19 @@ rm -rf ~/Library/Caches/* 2>/dev/null
 rm -rf ~/.Trash/* 2>/dev/null
 rm -rf ~/Library/Metadata/* 2>/dev/null
 rm -rf ~/Library/DuetExpertCenter/* 2>/dev/null
+
+if [ "$PURGE" = true ]; then
+    echo -e "[${BLUE}INFO${NC}] Purge mode enabled..."
+
+    rm -rf ~/Library/Group\ Containers/group.com.apple.CoreSpeech/ 2>/dev/null
+    rm -rf ~/Library/Group\ Containers/6N38VWS5BX.ru.keepcoder.Telegram/stable/logs/ 2>/dev/null
+    rm -rf ~/Library/Group\ Containers/6N38VWS5BX.ru.keepcoder.Telegram/stable/account-*/postbox/media/ 2>/dev/null
+    rm -rf ~/Library/Containers/com.apple.AvatarUI.AvatarPickerMemojiPicker/Data/Library/Caches/ 2>/dev/null
+    rm -rf ~/Library/Containers/com.apple.mediaanalysisd/Data/Library/Caches/ 2>/dev/null
+    rm -rf ~/Library/Application\ Support/Google/Chrome/Default/Service\ Worker/CacheStorage/* 2>/dev/null
+    rm -rf ~/Library/Application\ Support/Code/CachedData/* 2>/dev/null
+    rm -rf ~/Library/Logs/* 2>/dev/null
+fi
 
 FREE_AFTER=$(df -k ~ | tail -1 | awk '{print $4}')
 
